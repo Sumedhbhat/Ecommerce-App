5dade04f028d2a9857d6706c21d0de547aa6b1a5
sumedh-ubuntu <sumedhbhat01@gmail.com>
2021-08-09 23:34:24 +0530

    Revert "Finally final draft"
    This reverts commit 166aab86583acc0596427ce5304eabb45dbdbc94.

--- action.php
+++ action.php
@@ -243,7 +243,7 @@ if (isset($_POST["Common"])) {
 	if (isset($_POST["checkOutDetails"])) {
 		if (mysqli_num_rows($query) > 0) {
 			//display user cart item with "Ready to checkout" button if user is not login
-			echo "<form method='post' action='cart_process1.php'>";
+			echo "<form method='post' action='cart_process.php'>";
 				$n=0;
 				while ($row=mysqli_fetch_array($query)) {
 					$n++;
@@ -284,7 +284,38 @@ if (isset($_POST["Common"])) {
 
 
 					
-				}
+				}/*else if(isset($_SESSION["uid"])){
+					//Paypal checkout form
+					echo '
+						</form>
+						<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
+							<input type="hidden" name="cmd" value="_cart">
+							<input type="hidden" name="business" value="shoppingcart@ecommerceastro.com">
+							<input type="hidden" name="upload" value="1">';
+							  
+							$x=0;
+							$sql = "SELECT a.product_id,a.product_title,a.product_price,a.product_image,b.id,b.qty FROM products a,cart b WHERE a.product_id=b.p_id AND b.user_id='$_SESSION[uid]'";
+							$query = mysqli_query($con,$sql);
+							while($row=mysqli_fetch_array($query)){
+								$x++;
+								echo  	
+									'<input type="hidden" name="item_name_'.$x.'" value="'.$row["product_title"].'">
+								  	 <input type="hidden" name="item_number_'.$x.'" value="'.$x.'">
+								     <input type="hidden" name="amount_'.$x.'" value="'.$row["product_price"].'">
+								     <input type="hidden" name="quantity_'.$x.'" value="'.$row["qty"].'">';
+								}
+							  
+							echo   
+								'<input type="hidden" name="return" value="http://localhost/project1/payment_success.php"/>
+					                <input type="hidden" name="notify_url" value="http://localhost/ecommerce-app-h/payment_success.php">
+									<input type="hidden" name="cancel_return" value="http://localhost/ecommerce-app-h/cancel.php"/>
+									<input type="hidden" name="currency_code" value="USD"/>
+									<input type="hidden" name="custom" value="'.$_SESSION["uid"].'"/>
+									<input style="float:right;margin-right:80px;" type="image" name="submit"
+										src="https://www.paypalobjects.com/webstatic/en_US/i/btn/png/blue-rect-paypalcheckout-60px.png" alt="PayPal Checkout"
+										alt="PayPal - The safer, easier way to pay online">
+								</form>';
+				}*/
 			}
 	}
 	
